196baf1b74dd092bb4b88e5f5ba1aaaeddfc8a9e
Aleks <alekkswithak@gmail.com>
2020-11-18 13:14:37 +0000

    Spacy tests

--- document_analysis/word_sentences/tests.py
+++ document_analysis/word_sentences/tests.py
@@ -1,9 +1,40 @@
 from django.test import TestCase
+from spacy.lang.en import English
+from spacy.lang.en.stop_words import STOP_WORDS
 from .models import (
     Word,
     Sentence,
     Document
 )
+
+
+class TestSpacy(TestCase):
+    """Exploratory tests"""
+
+    def test_sentence_tokenization(self):
+        nlp = English()
+        nlp.add_pipe(nlp.create_pipe('sentencizer'))
+        doc = nlp("This a sentence. This is another one.")
+        sentences = [sent.text for sent in doc.sents]
+        self.assertEqual(len(sentences), 2)
+        self.assertEqual(sentences[0], "This a sentence.")
+
+    def test_word_tokenization(self):
+        nlp = English()
+        doc = nlp("This is a single sentence.")
+        tokens = [token.text for token in doc if not token.is_punct]
+        self.assertEqual(len(tokens), 5)
+
+    def test_word_tokenization_with_stopwords(self):
+        nlp = English()
+        doc = nlp("This is a single sentence.")
+        tokens = [
+            token.text
+            for token in doc
+            if not token.is_punct
+            if token.text.lower() not in STOP_WORDS
+        ]
+        self.assertEqual(len(tokens), 2)
 
 
 class TestModels(TestCase):
