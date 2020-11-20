e1fdea80e9f50dde21f88ebf40324fa42ada4bd9
Aleks <alekkswithak@gmail.com>
2020-11-20 22:18:21 +0000

    Basic front end

--- document_analysis/word_sentences/models.py
+++ document_analysis/word_sentences/models.py
@@ -8,6 +8,12 @@ class Word(models.Model):
     def __str__(self):
         return self.text
 
+    def documents(self):
+        out_str = 'Found in: '
+        for d in self.document_set.all():
+            out_str += '{}, '.format(d.name)
+        return out_str[:-2]
+
 
 class Sentence(models.Model):
     text = models.TextField(unique=True)
--- document_analysis/word_sentences/views.py
+++ document_analysis/word_sentences/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Word
 
-# Create your views here.
+
+class WordListView(ListView):
+    model = Word
+    queryset = Word.objects.all()
+    template_name = 'word_list.html'
+    paginate_by = 10
